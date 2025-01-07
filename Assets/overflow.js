3cdb79374e8a5514af0a9b1a41baa54f9fc9151c
Osmar Junior <orgj@protonmail.com>
2025-01-07 12:43:51 -0300

    Removes some bloat and corrects the message container style.

--- Assets/overflow.js
+++
@@ -1,7 +0,0 @@
-const superior = document.getElementById("right");
-const scrollableDiv = document.getElementById("messages");
-
-// Sincronizar rolagem da .scrollable-div com a .superior
-superior.addEventListener("scroll", () => {
-  scrollableDiv.scrollTop = superior.scrollTop;
-});
